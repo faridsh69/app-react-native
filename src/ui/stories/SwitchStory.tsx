4a82bd0dc0a37e1b06da5371ab2b7889b197b7d6
Farid Shahidi <farid.sh69@gmail.com>
2026-04-17 11:35:26 +0200

    Added more story to switch

--- src/ui/stories/SwitchStory.tsx
+++ src/ui/stories/SwitchStory.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react'
-import { View } from 'react-native'
+import { ScrollView } from 'react-native'
 
 import { AppSwitch } from '../Switch/Switch'
 
-export const SwitchStory = () => {
-  const [enabled, setEnabled] = useState(true)
+export default function SwitchStoriesScreen() {
+  const [a, setA] = useState(true)
+  const [b, setB] = useState(false)
+  const [c, setC] = useState(true)
 
   return (
-    <View style={{ padding: 16 }}>
-      <AppSwitch label='Face ID' description='Use Face ID to unlock the app' value={enabled} onChange={setEnabled} />
-    </View>
+    <ScrollView contentContainerStyle={{ padding: 16, gap: 24 }}>
+      <AppSwitch label='Notifications' description='Receive updates and alerts' value={a} onChange={setA} />
+
+      <AppSwitch label='Biometric login' description='Use Face ID to sign in' value={b} onChange={setB} size='sm' />
+
+      <AppSwitch label='Required setting' value={c} onChange={setC} required />
+
+      <AppSwitch label='Disabled' description='Cannot be changed right now' value onChange={() => {}} disabled />
+
+      <AppSwitch label='Error state' value={false} onChange={() => {}} hasError errorText='This field has an error' />
+    </ScrollView>
   )
 }
